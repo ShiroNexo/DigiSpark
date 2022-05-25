48ed1633f54a3118033855b1226fa185c02a93a8
ShiroNexo <106244851+ShiroNexo@users.noreply.github.com>
2022-05-25 19:53:13 +0700

    Add files via upload

---
+++ RickRoll_Schedule/rickroll.ps1
@@ -0,0 +1,5 @@
+﻿Add-Type -AssemblyName System.speech
+$speak = New-Object System.Speech.Synthesis.SpeechSynthesizer
+$speak.Speak("error, error, error, error, error, error, error, error. playing a music.")
+start "https://www.youtube.com/watch?v=oHg5SJYRHA0"
+exit
