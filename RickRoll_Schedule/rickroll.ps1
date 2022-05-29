82093a505bd4b706e0fa1c2eec1ed0709e0ca3d8
ShiroNexo <106244851+ShiroNexo@users.noreply.github.com>
2022-05-29 22:27:42 +0700

    Delete rickroll.ps1

--- RickRoll_Schedule/rickroll.ps1
+++
@@ -1,5 +0,0 @@
-﻿Add-Type -AssemblyName System.speech
-$speak = New-Object System.Speech.Synthesis.SpeechSynthesizer
-$speak.Speak("error, error, error, error, error, error, error, error. playing a music.")
-start "https://www.youtube.com/watch?v=oHg5SJYRHA0"
-exit
